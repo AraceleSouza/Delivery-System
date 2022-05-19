18001f269892f7b0577a355b38e014579fdbcd90
Aracele Souza <aracele-souza@hotmail.com>
2022-05-19 11:46:07 -0300

    Visitante visita home page

---
+++ spec/system/visitor_visits_homepage_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'visitor visits home page' do
+  it 'successfully' do
+    # Arrange
+
+    # Act
+      visit root_path
+    # Assert
+      expect(page).to have_content 'Sistema de Entregas'
+  end
+
+  
+end
