65cad022632cf23946cfdaaed14a8ceb275c25c3
Aracele Souza <aracele-souza@hotmail.com>
2022-05-21 18:40:34 -0300

    Configuração Devise para funcionário

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :functionaries
   devise_for :admins
   root to: 'home#index'
   resources :shipping_companies, only: [:index, :new, :create, :show, :edit, :update]
---
+++ spec/models/functionary_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Functionary, type: :model do
+  
+end
