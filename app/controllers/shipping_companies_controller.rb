639a1294e9f980665e0d04e4c7800ba73b720e34
Aracele Souza <aracele-souza@hotmail.com>
2022-05-20 23:11:52 -0300

    Usuário cadastra uma transportadora com sucesso

--- app/controllers/shipping_companies_controller.rb
+++ app/controllers/shipping_companies_controller.rb
@@ -6,4 +6,21 @@ class ShippingCompaniesController < ApplicationController
   def new
     @shipping_company = ShippingCompany.new
   end
+
+  def show
+    @shipping_company = ShippingCompany.find(params[:id])
+  end
+
+  def create
+    @shipping_company = ShippingCompany.new(params.require(:shipping_company).permit(:corporate_name, :fantasy_name, :email, 
+    :cnpj , :address, :city, :state, :cep))
+    if @shipping_company.save()
+      redirect_to shipping_companies_path, notice: "Transportadora cadastrada com sucesso."
+    else
+      flash.now[:notice] = "Transportadora não cadastrada."
+      render'new'
+    end
+  end
 end
+
+
