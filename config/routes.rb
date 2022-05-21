d51c652585e8ff5b00ff9784f14f501238d6fe2f
Aracele Souza <aracele-souza@hotmail.com>
2022-05-20 21:41:01 -0300

    Administrador vê detalhes de uma transportadora com informações adicionais

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins
   root to: 'home#index'
-  resources :shipping_companies, only: [:index, :new, :create]
+  resources :shipping_companies, only: [:index, :new, :create, :show]
 end
