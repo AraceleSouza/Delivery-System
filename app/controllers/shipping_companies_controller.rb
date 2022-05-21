e6d16b869b365a1c0f109c4a6144978a2ed2d9fd
Aracele Souza <aracele-souza@hotmail.com>
2022-05-20 23:57:38 -0300

    Administrador edita uma transportadora com sucesso

--- app/controllers/shipping_companies_controller.rb
+++ app/controllers/shipping_companies_controller.rb
@@ -12,14 +12,35 @@ class ShippingCompaniesController < ApplicationController
   end
 
   def create
-    @shipping_company = ShippingCompany.new(params.require(:shipping_company).permit(:corporate_name, :fantasy_name, :email, 
-    :cnpj , :address, :city, :state, :cep))
+    @shipping_company = ShippingCompany.new(shipping_company_params)
     if @shipping_company.save()
       redirect_to shipping_companies_path, notice: "Transportadora cadastrada com sucesso."
     else
       flash.now[:notice] = "Transportadora não cadastrada."
       render'new'
     end
+  end
+
+  def edit 
+    @shipping_company = ShippingCompany.find(params[:id])
+  end
+
+  def update
+    @shipping_company = ShippingCompany.find(params[:id])     
+    if @shipping_company.update(shipping_company_params)
+      redirect_to shipping_companies_path(@shipping_company.id), notice: 'Transportadora atualizada com sucesso.'
+    else
+      flash.now[:notice] = 'Não foi possível atualizar a transportadora.'
+      render'edit'
+    end
+  end
+
+  private
+
+  
+  def shipping_company_params
+    params.require(:shipping_company).permit(:corporate_name, :fantasy_name, :email, 
+    :cnpj , :address, :city, :state, :cep)
   end
 end
 
