11c49fe4f29c0a0a67d105019fa82901b57eac1d
Aracele Souza <aracele-souza@hotmail.com>
2022-05-24 23:18:10 -0300

    Funcionário cadastra preçoreço da transportadora com dados incompletos.

--- app/models/price_setting.rb
+++ app/models/price_setting.rb
@@ -1,3 +1,4 @@
 class PriceSetting < ApplicationRecord
   belongs_to :shipping_company
+  validates :min_weight, :max_weight, :min_cubic_meter, :max_cubic_meter , :distance_value, presence: true
 end
--- spec/system/functionary/functionary_registers_price_spec.rb
+++ spec/system/functionary/functionary_registers_price_spec.rb
@@ -29,4 +29,34 @@ describe 'Functionary registers shipping company prices' do
   expect(page).to have_content 'Metros cubicos Máximo: 2,5 metros'
   expect(page).to have_content 'Valor por KM: R$ 190,00'                               
   end
+
+  
+  it 'with incomplete data' do 
+     # Arrange
+  shipping_company = ShippingCompany.create!(corporate_name: 'Transportadora Imperial do Brasil LTDA', 
+                                              fantasy_name: 'Transportadora Imperial', 
+                                              email: 'contato@transportadoraimperial.com', 
+                                              cnpj: '62325611000167', address:'Av das Flores, 108', 
+                                              city: 'Cajamar', state:'SP' , cep: '12536-100' )
+  functionary = Functionary.create!(email: 'eduarda@transportadoraimperial.com', password: 'password', 
+                                    shipping_company: shipping_company)
+  # Act
+  login_as(functionary)
+  visit shipping_company_price_settings_path(shipping_company.id)
+  click_on 'Cadastrar Preço' 
+  fill_in 'Peso Mínimo', with: ' '
+  fill_in 'Peso Máximo', with: ' '
+  fill_in 'Metros cubicos Mínimo', with: ' '
+  fill_in 'Metros cubicos Máximo', with: ' '
+  fill_in 'Valor por KM', with: ''
+  click_on 'Enviar'
+  # Assert
+  expect(current_path).to eq shipping_company_price_settings_path(shipping_company.id)
+  expect(page).to have_content 'Preço não cadastrado.'
+  expect(page).to have_content "Peso Mínimo não pode ficar em branco" 
+  expect(page).to have_content "Peso Máximo não pode ficar em branco"
+  expect(page).to have_content "Metros cubicos Mínimo não pode ficar em branco"
+  expect(page).to have_content "Metros cubicos Máximo não pode ficar em branco"
+  expect(page).to have_content "Valor por KM não pode ficar em branco"
+  end
 end
