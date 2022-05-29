5398c12b52c8c2c79bc31539d1caa87ea565818e
Aracele Souza <aracele-souza@hotmail.com>
2022-05-29 16:28:49 -0300

    Administrador vê detalhes do pedido

--- spec/system/admin/service_order/admin_view_own_service_orders_spec.rb
+++ spec/system/admin/service_order/admin_view_own_service_orders_spec.rb
@@ -26,10 +26,34 @@ describe 'Admin sees their own orders' do
     # Arrange 
     admin = Admin.create!(email: 'aracele@email.com', password: '123789')
     # Act
-    login_as(admin)
+    login_as(admin, :scope => :admin)
     visit root_path
     click_on 'Meus Pedidos'
     # Arrange 
     expect(page).to have_content('Não existem ordens de serviço cadastradas.')
+  end
+
+  it 'and view order details' do
+    # Arrange
+    admin = Admin.create!(email: 'aracele@email.com', password: '123789')
+    shipping_company = ShippingCompany.create!(corporate_name: 'Alternativa Express LTDA', fantasy_name: 'Alternativa Express', 
+                                              email: 'agendamento@alternativa.com ', cnpj: '43835515000114', 
+                                              address:'Rua Conde do Pinhal, 56', city: 'Guarulhos', state:'SP', cep:'12369-122' )  
+    vehicle = Vehicle.create!(plate: 'JSQ-7436', brand: 'Mercedes-Benz', year_fabrication: '2019',
+                              model: 'Sprinter Chassi',freight: '1.840', shipping_company: shipping_company)
+    product_model = ProductModel.create!(name: 'Impressora HP' , weight: 4000 , width: 40 , height: 18 , depth: 35,
+                                        sku: 'IMPRESHP9563625' , shipping_company:shipping_company)
+    service_order = ServiceOrder.create!(shipping_company: shipping_company, vehicle: vehicle, product_model: product_model,
+                                                estimated_delivery_date: 1.day.from_now)
+    # Act
+    login_as(admin, :scope => :admin)
+    visit root_path
+    click_on 'Meus Pedidos'
+    click_on service_order.code
+    # Assert
+    expect(page).to have_content 'Detalhes do Pedido'
+    expect(page).to have_content service_order.code
+    expect(page).to have_content 'Transportadora: Alternativa Express LTDA'
+    expect(page).to have_content 'Veículo: Sprinter Chassi'
   end
 end
