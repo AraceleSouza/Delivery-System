8fdcc6d0e180dbf37d227a1112352684bbb2c2d7
Aracele Souza <aracele-souza@hotmail.com>
2022-05-30 14:16:59 -0300

    Testes para validações específicas

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,4 +1,5 @@
 class ProductModel < ApplicationRecord
   belongs_to :shipping_company
   validates :name, :weight, :width, :height, :depth, :sku, presence: true
+  validates :sku, uniqueness: true
 end
--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -18,6 +18,66 @@ RSpec.describe ProductModel, type: :model do
         expect(result).to eq false 
       end
 
+      it 'false when weight is empty' do
+        # Arrange
+        shipping_company = ShippingCompany.create!(corporate_name: 'Transportadora Imperial do Brasil LTDA', 
+                                                fantasy_name: 'Transportadora Imperial', 
+                                                email: 'contato@transportadoraimperial.com', 
+                                                cnpj: '62325611000167', address:'Av das Flores, 108', 
+                                                city: 'Cajamar', state:'SP' , cep: '12536-100' )
+        product_model = ProductModel.new(name: 'Notebook Dell' , width: 35 , height: 25 , 
+                                              depth: 10 , sku: 'NOTEDELL0905697' , shipping_company: shipping_company)
+        # Act
+        result = product_model.valid?
+        # Assert
+        expect(result).to eq false 
+      end
+      
+      it 'false when width is empty' do
+        # Arrange
+        shipping_company = ShippingCompany.create!(corporate_name: 'Transportadora Imperial do Brasil LTDA', 
+                                                fantasy_name: 'Transportadora Imperial', 
+                                                email: 'contato@transportadoraimperial.com', 
+                                                cnpj: '62325611000167', address:'Av das Flores, 108', 
+                                                city: 'Cajamar', state:'SP' , cep: '12536-100' )
+        product_model = ProductModel.new(name: 'Notebook Dell' , weight: 1700  , height: 25 , 
+                                              depth: 10 , sku: 'NOTEDELL0905697' , shipping_company: shipping_company)
+        # Act
+        result = product_model.valid?
+        # Assert
+        expect(result).to eq false 
+      end
+
+      it 'false when height is empty' do
+        # Arrange
+        shipping_company = ShippingCompany.create!(corporate_name: 'Transportadora Imperial do Brasil LTDA', 
+                                                fantasy_name: 'Transportadora Imperial', 
+                                                email: 'contato@transportadoraimperial.com', 
+                                                cnpj: '62325611000167', address:'Av das Flores, 108', 
+                                                city: 'Cajamar', state:'SP' , cep: '12536-100' )
+        product_model = ProductModel.new(name: 'Notebook Dell' , weight: 1700 , width: 35 , 
+                                              depth: 10 , sku: 'NOTEDELL0905697' , shipping_company: shipping_company)
+        # Act
+        result = product_model.valid?
+        # Assert
+        expect(result).to eq false 
+      end
+
+      it 'false when depth is empty' do
+        # Arrange
+        shipping_company = ShippingCompany.create!(corporate_name: 'Transportadora Imperial do Brasil LTDA', 
+                                                fantasy_name: 'Transportadora Imperial', 
+                                                email: 'contato@transportadoraimperial.com', 
+                                                cnpj: '62325611000167', address:'Av das Flores, 108', 
+                                                city: 'Cajamar', state:'SP' , cep: '12536-100' )
+        product_model = ProductModel.new(name: 'Notebook Dell' , weight: 1700 , width: 35 , height: 25 , 
+                                              sku: 'NOTEDELL0905697' , shipping_company: shipping_company)
+        # Act
+        result = product_model.valid?
+        # Assert
+        expect(result).to eq false 
+      end
+
       it 'false when sku is empty' do
         # Arrange
         shipping_company = ShippingCompany.create!(corporate_name: 'Transportadora Imperial do Brasil LTDA', 
@@ -33,5 +93,23 @@ RSpec.describe ProductModel, type: :model do
         expect(result).to eq false 
       end
     end 
+
+    it 'false when sku is already in use' do
+      # Arrange
+      shipping_company = ShippingCompany.create!(corporate_name: 'Alternativa Express LTDA', fantasy_name: 'Alternativa Express', 
+                              email: 'agendamento@alternativa.com ', cnpj: '43835515000114', 
+                              address:'Rua Conde do Pinhal, 56', city: 'Guarulhos', state:'SP', 
+                              cep:'12369-122' )
+      first_product_model = ProductModel.create!(name: 'Notebook Dell', weight: 1700, width: 35, 
+                                              height: 25, depth: 10 , sku: 'NOTEDELL0905697', 
+                                              shipping_company: shipping_company)
+      second_product_model = ProductModel.new(name: 'Impressora HP', weight: 4000, width: 40, 
+                                                  height: 18, depth: 35, sku: 'NOTEDELL0905697', 
+                                                  shipping_company: shipping_company)                                       
+      # Act
+      result = second_product_model.valid?
+      # Assert
+      expect(result).to eq false
+    end
   end 
 end
